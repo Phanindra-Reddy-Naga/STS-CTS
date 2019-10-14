0f34436aee1f72e8ff1dd74bcb5d5e3b067d1301
760140 <760140@PC369162.cts.com>
2019-10-14 13:08:21 +0530

    Commit all files

---
+++ MyWebApp/src/main/java/com/phani/demo/HomeController.java
@@ -0,0 +1,14 @@
+package com.phani.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.*;
+
+@Controller
+public class HomeController {
+
+	public void home()
+	{
+		System.out.println("c");
+	}
+
+}
